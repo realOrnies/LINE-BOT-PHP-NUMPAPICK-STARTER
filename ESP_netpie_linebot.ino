8adf5c7a2b72deb47027d5faca791bc2a848e727
realOrnies <47171844+realOrnies@users.noreply.github.com>
2019-02-15 22:56:21 +0700

    Update ESP_netpie_linebot.ino

--- ESP_netpie_linebot.ino
+++ ESP_netpie_linebot.ino
@@ -2,8 +2,8 @@
 #include <MicroGear.h>
 #include <ESP8266HTTPClient.h>
 #include <ArduinoJson.h>
-const char* ssid     = "ornxviii"; //change this to your SSID
-const char* password = "09062541"; //change this to your PASSWORD
+const char* ssid     = "your SSID"; //change this to your SSID
+const char* password = "your PASSWORD"; //change this to your PASSWORD
 
 const char* host = "https://malii.herokuapp.com/bot.php";//change this to your linebot server ex.http://numpapick-linebot.herokuapp.com/bot.php
 #define APPID   "your APPID"     //change this to your APPID
