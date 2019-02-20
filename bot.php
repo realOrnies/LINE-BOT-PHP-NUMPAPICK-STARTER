fb13c01c88c50a7f1ef7b3295945cebae04a4d3a
realOrnies <47171844+realOrnies@users.noreply.github.com>
2019-02-21 01:34:58 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -7,7 +7,7 @@ $content = file_get_contents('php://input');
 // Parse JSON
 
 $events = json_decode($content, true);
-echo events;
+//echo events;
 // Validate parsed JSON data
 if (!is_null($events["ESP"])) {
 	
