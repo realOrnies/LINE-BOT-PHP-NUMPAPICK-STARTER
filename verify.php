d41484ccbadc2fc1b8468ab08ff30261c00d1a64
realOrnies <47171844+realOrnies@users.noreply.github.com>
2019-02-21 01:41:48 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,6 +1,5 @@
 <?php
-$access_token = '2L/dqzBLE8WYVSU/rktq/ak6+ygLTKqvTKq9ziQuGj7zBwMAZvgSm57p6yecAD8Dek+CQY5u1qZBDV72hLwITp7hna1dwENAIYV9+AoUlnmqsCCdsurc6Fh0D0ZZBKEZCGALePrVsEbV2CS23RH7uQdB04t89/1O/w1cDnyilFU=
-'
+$access_token = 'WpU/RYEmb0k4QQy2982hTuTIIP8+s9OZWzwf0Q4wBwt51Aa60D4oT6I4guFi27+0ek+CQY5u1qZBDV72hLwITp7hna1dwENAIYV9+AoUlnm8b3yj5s8R1AU66dw6Ui2Cqg0WsheZessppVVh4mQwzwdB04t89/1O/w1cDnyilFU=';
 $url = 'https://api.line.me/v1/oauth/verify';
 
 $headers = array('Authorization: Bearer ' . $access_token);
